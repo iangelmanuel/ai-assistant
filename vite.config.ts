468c6c6fb286c4b4ff14303027587697b6501bba
iAngelManuel <iangelmanueldm@gmail.com>
2024-06-13 04:19:40 -0500

    chore: Update path aliases in vite.config.ts, components.json, tailwindcss to aplicate shadcn

--- vite.config.ts
+++ vite.config.ts
@@ -1,4 +1,5 @@
 import react from '@vitejs/plugin-react-swc'
+import path from 'path'
 import { defineConfig } from 'vite'
 
 // https://vitejs.dev/config/
@@ -6,7 +7,7 @@ export default defineConfig({
   plugins: [react()],
   resolve: {
     alias: {
-      '@': '/src'
+      '@': path.resolve(__dirname, './src')
     }
   }
 })
