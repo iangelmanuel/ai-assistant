51062e5c79259e25347121a55657c6581221e885
iAngelManuel <iangelmanueldm@gmail.com>
2024-06-13 04:21:56 -0500

    chore: Adding theme provider and toaster with sonner

--- src/main.tsx
+++ src/main.tsx
@@ -1,10 +1,15 @@
+import { Toaster } from '@/components/ui/sonner'
+import { ThemeProvider } from '@/providers'
+import '@/styles/index.css'
 import React from 'react'
 import ReactDOM from 'react-dom/client'
-import App from './App.tsx'
-import './index.css'
+import App from './App'
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
   <React.StrictMode>
-    <App />
+    <ThemeProvider>
+      <App />
+    </ThemeProvider>
+    <Toaster richColors />
   </React.StrictMode>
 )
