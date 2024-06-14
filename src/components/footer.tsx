95c9ae9f0c789d20354f865dff47aab5639dbf20
iAngelManuel <iangelmanueldm@gmail.com>
2024-06-14 17:43:09 -0500

    chore: Update footer styling and add margin-top to improve spacing

--- src/components/footer.tsx
+++ src/components/footer.tsx
@@ -1,7 +1,7 @@
 export const Footer = () => {
   return (
-    <footer className="flex justify-center items-end p-5">
-      With 🫶 by Angel M.
+    <footer className="flex justify-center items-end p-5 mt-20">
+      <p>With 🫶 by Angel M.</p>
     </footer>
   )
 }
