c50ac4bfa1581947fb56d74dd70fb1cd142fd5ec
iAngelManuel <iangelmanueldm@gmail.com>
2024-06-14 19:23:30 -0500

    chore: Trim chat message content and update maximum character limit to 500

--- src/scheme/index.ts
+++ src/scheme/index.ts
@@ -4,6 +4,7 @@ export const ChatScheme = z.object({
   role: z.enum(['user', 'system'], { message: 'El rol es requerido' }),
   content: z
     .string({ message: 'El mensaje es requerido' })
+    .trim()
     .min(1, { message: 'El mensaje debe tener minimo 1 caracter' })
     .max(500, { message: 'El mensaje debe tener menos de 500 caracteres' })
 })
